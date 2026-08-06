4013f9be7dfbc62a5589f7339d9b61b3a0dbd9ce
Jean-Christian Denis <contact@jcdenis.fr>
2026-08-06 23:15:04 +0200

    fix path

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 
 cd /data
 echo "> Preparing credentials"
-sed -i -e "s|DKNR_USERNAME|${DKNR_USERNAME}|g" /settings.js
-sed -i -e "s|DKNR_HASH|$(expr substr "$(echo "${DKNR_PASSWORD}" | node-red admin hash-pw)" 11 100)|g" /settings.js
-sed -i -e "s|DKNR_SECRET|${DKNR_SECRET}|g" /settings.js
+sed -i -e "s|DKNR_USERNAME|${DKNR_USERNAME}|g" settings.js
+sed -i -e "s|DKNR_HASH|$(expr substr "$(echo "${DKNR_PASSWORD}" | node-red admin hash-pw)" 11 100)|g" settings.js
+sed -i -e "s|DKNR_SECRET|${DKNR_SECRET}|g" settings.js
 echo "> Instaling packages"
 npm install --unsafe-perm --no-update-notifier --no-fund --omit=dev
 echo "> Running node-red"
